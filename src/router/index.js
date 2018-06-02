d515841f2655084e39b72b4c4cee95b8e4b7262e
plasusu <zhaobin0511@qq.com>
2018-06-02 14:29:26 +0800

    update

---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import home from 'pages/home'
+import rank from 'pages/rank'
+import search from 'pages/search'
+
+Vue.use(Router)
+
+export default new Router({
+  linkActiveClass: 'active',
+  routes: [
+    {
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: home
+    },
+    {
+      path: '/rank',
+      name: 'rank',
+      component: rank
+    },
+    {
+      path: '/search',
+      name: 'search',
+      component: search
+    }
+  ]
+})
