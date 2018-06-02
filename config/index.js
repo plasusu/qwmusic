f894002c58207426f249304bb715dc6a41e3d592
plasusu <zhaobin0511@qq.com>
2018-06-02 15:24:55 +0800

    update

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'music/static',
+    assetsSubDirectory: '',
     assetsPublicPath: '/',
 
     /**
